5748203604ad52effa5df082fb4503a5a8e55e97
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-08-02 16:29:22 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,6 @@
 
 const datata = ['default', 'angry', 'happy', 'sad']
 
-console.log(`I am registered at ${self.registration.scope}`)
-
 self.addEventListener('install', event => {
   const mountpoint = new URL(self.registration.scope).pathname.slice(0, -1);
   const toCache = [
