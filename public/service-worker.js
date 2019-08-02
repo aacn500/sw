124b5cc521124dc65a9b3d6e2f1bb21cc82c9d7a
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-08-02 16:36:41 +0100

    woo its working again

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,26 @@
 /* eslint-disable no-restricted-globals */
+
+const datata = ['default', 'angry', 'happy', 'sad']
+
 self.addEventListener('install', event => {
+  const mountpoint = new URL(self.registration.scope).pathname.slice(0, -1);
+  const toCache = [
+    `${mountpoint}`,
+    ...datata.map(datum => `${mountpoint}/data/${datum}.json`),
+  ];
+  console.log(toCache)
+
   event.waitUntil(
     caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/data/default.json',
-        '/data/angry.json',
-        '/data/happy.json',
-        '/data/sad.json'
-      ]);
+      return cache.addAll(toCache);
     })
   );
 })
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   )
 })
