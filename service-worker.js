99f5268df6fe829cf4b44a479863fcea4b7c33fc
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-08-02 14:47:27 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -4,19 +4,17 @@ self.addEventListener('install', event => {
     caches.open('v1').then(function(cache) {
       return cache.addAll([
         '/',
+        '/data/default.json',
+        '/data/angry.json',
+        '/data/happy.json',
+        '/data/sad.json'
       ]);
     })
   );
 })
 
 self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-  if (url.pathname.includes('.')) return;
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || new Response('["you", "got", "service", "workered"]', {
-        headers: { 'Content-Type': 'application/json; charset=UTF-8' }
-      });
-    })
+    caches.match(event.request)
   )
 })
