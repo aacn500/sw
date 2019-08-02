e983c5926097a0c9b7dd1cdb08158cb53d11b5b9
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-08-02 14:06:20 +0100

    this no longer kills the page yay

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,5 +10,13 @@ self.addEventListener('install', event => {
 })
 
 self.addEventListener('fetch', event => {
-  event.respondWith(new Response('["you","got","service","workered"]'))
+  const url = new URL(event.request.url);
+  if (url.pathname.includes('.')) return;
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || new Response('["you", "got", "service", "workered"]', {
+        headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+      });
+    })
+  )
 })
